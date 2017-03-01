a881d931855577f1cbe34a6b5ce696f90d1090f4
Devash Anandpara <Devash Anandpara@DESKTOP-NDB0GV0>
2017-03-01 12:25:23 -0800

    Add project files.

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.Models;
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            string welcomeMessage = "";
+            DateTime date = DateTime.Now;
+            int hour = date.Hour;
+            if (hour < 12)
+                welcomeMessage = "Good Morning!";
+            else if (hour < 18)
+                welcomeMessage = "Good afternoon!";
+            else
+                welcomeMessage = "Good evening!";
+            welcomeMessage += Environment.NewLine;
+            welcomeMessage += "The time is " + String.Format("{0:t}", date) + " on " +
+                String.Format("{0:dddd, MMMM d, yyyy}", date) ;
+            DateTime nextYear = new DateTime(date.Year + 1, 1, 1);
+            int DaysLeft = nextYear.Subtract(date).Days;
+            welcomeMessage += Environment.NewLine;
+            welcomeMessage += DaysLeft + " more days until " + date.Year + 1;
+            ViewData["WelcomeMessage"] = welcomeMessage;
+            return View();
+        }
+        public ActionResult ShowPerson()
+        {
+            Person person = new Person() { FirstName="Devash", LastName="Anandpara", DateOfBirth= new DateTime(1994, 13, 11)
+                                          };
+            ViewData["Person"] = person;
+            return View(person);
+        }
+
+        public ActionResult AddPerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddPerson(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["Person"] = person;
+                return View("ShowPerson", person);
+            }
+            else
+                return View();
+        }
+    }
+}
---
+++ MVCDemo/Models/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCDemo.Models
+{
+    public class Person
+    {
+        [Required][Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(20,ErrorMessage ="Last Name length should be between 2 and 20"),MinLength(2)]
+        public string LastName { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Date of Birth")]
+        public DateTime DateOfBirth { get; set; }
+        public int Age
+        {
+            get
+            {
+                DateTime now = DateTime.Today;
+                int age = now.Year - DateOfBirth.Year;
+                return age;
+            }
+        }
+    }
+    
+}
